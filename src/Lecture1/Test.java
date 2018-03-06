d82a2e929319d668b61b077ba0813f49a40fc3cc
Garima Chhikara <garimachhikara128@gmail.com>
2018-03-06 20:43:11 +0530

    Pattern Code

--- src/Lecture1/Test.java
+++ src/Lecture1/Test.java
@@ -10,7 +10,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		// System.out.print("Hello World");
+		// System.out.print("Hello World"); 
 		// System.out.print("\n");
 		// System.out.print("Hello World");
 
