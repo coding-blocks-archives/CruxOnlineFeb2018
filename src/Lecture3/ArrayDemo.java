7c601340a4a383b0e5883c8563dcadc01c6e40b8
Garima Chhikara <garimachhikara128@gmail.com>
2018-03-08 18:04:56 +0530

    Lecture 3 Code

---
+++ src/Lecture3/ArrayDemo.java
@@ -0,0 +1,46 @@
+package Lecture3;
+
+/**
+ * @author Garima Chhikara
+ * @email garima.chhikara@codingblocks.com
+ * @course Online Java DSA
+ */
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		int[] arr = null;
+		System.out.println(arr);
+
+		arr = new int[5];
+		System.out.println(arr);
+
+		System.out.println(arr.length);
+
+		// get
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+
+		// set
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+
+		// get
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+
+		// System.out.println(arr[5]);
+
+	}
+
+}
